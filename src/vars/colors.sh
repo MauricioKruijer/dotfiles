789b4ae2c57b3170624536bd4ca7720577d2f33d
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2017-11-22 23:36:10 +0100

    Test out install script

---
+++ src/vars/colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tput sgr0
+COLOR_RED=$(tput setaf 1)
+COLOR_ORANGE=$(tput setaf 3)
+COLOR_GREEN=$(tput setaf 2)
+COLOR_PURPLE=$(tput setaf 5)
+COLOR_BLUE=$(tput setaf 4)
+COLOR_WHITE=$(tput setaf 7)
+COLOR_BOLD=$(tput bold)
+COLOR_RESET=$(tput sgr0)
+COLOR_YELLOW="JE DIKKE "
